6a299d74fccb763a66e48166d8156b9d962533a1
jameshlms <103217525+jameshlms@users.noreply.github.com>
2026-01-29 12:41:13 -0500

    Made script to get raw data

---
+++ setup.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def main():
+    r = requests.get(
+        "https://www.fisheries.noaa.gov/s3/2023-06/EastCoast-Bottlenose-2017-2019.xlsx"
+    )
+
+    with open("data/raw/EastCoast-Bottlenose-2017-2019.xlsx", "wb") as f:
+        f.write(r.content)
+
+    r.close()
+
+
+if __name__ == "__main__":
+    main()
